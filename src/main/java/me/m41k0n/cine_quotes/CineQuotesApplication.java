d05dc5c5fca75fd8ae02dcdeb1c610ce7ac57649
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2025-01-27 03:00:00 -0300

    first commit

---
+++ src/main/java/me/m41k0n/cine_quotes/CineQuotesApplication.java
@@ -0,0 +1,13 @@
+package me.m41k0n.cine_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/m41k0n/cine_quotes/CineQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.m41k0n.cine_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
