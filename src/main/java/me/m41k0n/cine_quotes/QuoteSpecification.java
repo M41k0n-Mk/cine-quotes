982b03438e848551986103af8155980aed51ea1e
M41k0n <142348031+M41k0n-Mk@users.noreply.github.com>
2025-01-28 03:53:07 -0300

    refact: remove duplicate dependencies and other unnecessary stuff

--- src/main/java/me/m41k0n/cine_quotes/QuoteSpecification.java
+++ src/main/java/me/m41k0n/cine_quotes/QuoteSpecification.java
@@ -2,7 +2,6 @@ package me.m41k0n.cine_quotes;
 
 import org.springframework.data.jpa.domain.Specification;
 
-
 public class QuoteSpecification {
     public static Specification<Quote> hasTitle(String title) {
         return (root, query, criteriaBuilder) ->
